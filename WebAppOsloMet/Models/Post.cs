feba010ce68030aac5816aa5633fce93ea9bdbcc
Kevin L. A <s364530@oslomet.no>
2023-10-30 14:28:56 +0100

    La til noen comments på views for nå
    Fjernet også _newComment og privacy filene
    La til en 80 character limit på title også, da 20 var for kort

--- WebAppOsloMet/Models/Post.cs
+++ WebAppOsloMet/Models/Post.cs
@@ -10,12 +10,14 @@ namespace WebAppOsloMet.Models
     {
         public int PostID { get; set; }
 
-        [RegularExpression(@"[0-9a-zA-Zæøå. \-]{2,20}", ErrorMessage = "The title must contain numbers " +
-            "or letters, and must be between 2 and 20 characters.")]
-        [Display(Name = "Post name")]
+        [RegularExpression(@"[0-9a-zA-Zæøå. \-]{2,80}", ErrorMessage = "The title must contain numbers " +
+            "or letters, and must be between 2 and 80 characters.")]
+        [Display(Name = "Title")]
         public string Title { get; set; } = string.Empty;
 
         [StringLength(200)]
+        [Display(Name = "Description")]
+        [Required]
         public string? Text { get; set; }
         public string? ImageUrl { get; set; }
         public string? PostDate { get; set; }
@@ -24,6 +26,7 @@ namespace WebAppOsloMet.Models
         public virtual List<Comment>? Comments { get; set; }
         public int UpvoteCount { get; set; } = 0;
         public virtual List<Upvote>? UserVotes { get; set; }
+        [Display(Name = "Subforum")]
         public string SubForum { get; set; } = string.Empty;
     }
 }
